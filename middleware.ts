a851c165a1c5031e6abd44a0cac70e610903d336
jawherbalti <jawher-balti@live.com>
2024-01-10 11:00:31 +0100

    first commit

---
+++ middleware.ts
@@ -0,0 +1,41 @@
+import authConfig from "./auth.config"
+import NextAuth from "next-auth"
+import {
+    DEFAULT_LOGIN_REDIRECT,
+    apiAuthPrefix,
+    authRoutes,
+    publicRoutes
+} from "@/routes"
+
+const { auth } = NextAuth(authConfig)
+
+export default auth((req) => {
+    const {nextUrl} = req
+    const isLoggedIn = !!req.auth
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+    if(isApiAuthRoute) return null
+    if(isAuthRoute) {
+        if(isLoggedIn) return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+        return null
+    }
+    if(!isLoggedIn && !isPublicRoute) {
+        let callbackUrl = nextUrl.pathname
+        if(nextUrl.search) {
+            callbackUrl+= nextUrl.search
+        }
+        const encodedUrl = encodeURIComponent(callbackUrl)
+        console.log(callbackUrl);
+        
+        console.log(encodedUrl)
+        return Response.redirect(new URL(`/auth/login?callbackUrl=${encodedUrl}`, nextUrl))
+    }
+
+    return null
+})
+
+export const config = {
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+}
