86453d48aafc28798ab3a21c77e67df8906a93be
jawherbalti <jawher-balti@live.com>
2024-01-11 09:52:03 +0100

    create course

---
+++ app/api/courses/route.ts
@@ -0,0 +1,24 @@
+import { currentUser } from "@/lib/auth"
+import { db } from "@/lib/db"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request) {
+    try {
+        const user = await currentUser()
+        const userId = user?.id
+
+        const { title } = await req.json()
+
+        if (!userId) return new NextResponse("Unauthorized", { status: 401 })
+
+        const course = await db.course.create({
+            data: {
+                userId,
+                title
+            }
+        })
+        return NextResponse.json(course)
+    } catch (error) {
+        return new NextResponse("Internal Error", { status: 500 })
+    }
+}
