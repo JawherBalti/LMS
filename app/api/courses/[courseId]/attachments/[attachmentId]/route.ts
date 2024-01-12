2e69a986dc5cac2b11f404c06e038445cfe5db5d
jawherbalti <jawher-balti@live.com>
2024-01-12 16:18:30 +0100

    course attachments

---
+++ app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
@@ -0,0 +1,31 @@
+import { currentUser } from '@/lib/auth';
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+export async function DELETE(req: Request, { params }: { params: { courseId: string, attachmentId: string } }) {
+    try {
+        const user = await currentUser()
+
+        if(!user?.id) return new NextResponse("Unauthorized", {status:401})
+
+        const courseOwner = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                userId: user.id
+            }
+        })
+
+        if(!courseOwner) return new NextResponse("Unauthorized", {status:401})
+
+        const attachment = await db.attachment.delete({
+            where: {
+                courseId: params.courseId,
+                id: params.attachmentId
+            }
+        })
+        return NextResponse.json(attachment)
+    } catch (error) {
+        return new NextResponse("Internale Error", { status: 500 })
+    }
+
+}
