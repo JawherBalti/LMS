227ca3c5c57580055063150b2909b2411a493aef
jawherbalti <jawher-balti@live.com>
2024-01-13 12:05:49 +0100

    add confetti effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { auth } from "@/auth";
 import { SessionProvider } from "next-auth/react";
 import { ToasterProvider } from "@/components/providers/toaster-provider";
+import { ConfettiProvider } from "@/components/providers/confetti-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default async function RootLayout({
     <SessionProvider session={session}>
       <html lang="en">
         <body className={inter.className}>
+          <ConfettiProvider/>
           <ToasterProvider />
           {children}
         </body>
