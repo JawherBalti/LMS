6ff54531c126842d4f0f72d18623289b31858349
jawherbalti <jawher-balti@live.com>
2024-01-15 14:31:45 +0100

    display created courses

--- app/(protected)/teacher/courses/page.tsx
+++ app/(protected)/teacher/courses/page.tsx
@@ -1,12 +1,28 @@
 import { Button } from "@/components/ui/button";
+import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const user = await currentUser();
+
+  if (!user?.id) return redirect("/");
+
+  const courses = await db.course.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
