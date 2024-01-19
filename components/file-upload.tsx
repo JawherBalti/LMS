fda7b5414d23f326c942eef1ec79e69f870ce26c
jawherbalti <jawher-balti@live.com>
2024-01-19 16:09:59 +0100

    change component style

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,6 +2,7 @@
 
 import { ourFileRouter } from "@/app/api/uploadthing/core";
 import { UploadDropzone } from "@/lib/uploadthing";
+import { usePathname } from "next/navigation";
 import toast from "react-hot-toast";
 
 interface FileUploadProps {
@@ -10,8 +11,11 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ onChange, endpoint }: FileUploadProps) => {
+  const pathname = usePathname()
+
   return (
     <UploadDropzone
+      className={pathname.includes("settings") ? "mt-0 flex items-center justify-center h-60 w-60 bg-slate-200 rounded-full border-none" : ""}
       endpoint={endpoint}
       onClientUploadComplete={(res) => onChange(res?.[0].url)}
       onUploadError={(error: Error) => toast.error(`${error.message}`)}
