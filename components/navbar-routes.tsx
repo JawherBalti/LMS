4aa6ec4f5e58fff2a566d0f1b1dff1d8cd9df63d
jawherbalti <jawher-balti@live.com>
2024-01-19 16:08:50 +0100

    search course

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,32 +5,41 @@ import { UserButton } from "./auth/user-button";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { ExitIcon, BackpackIcon } from "@radix-ui/react-icons";
+import SearchInput from "./search-input";
 
 export const NavbarRoutes = () => {
   const pathname = usePathname();
   const router = useRouter();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
-  const isPlayerPage = pathname?.startsWith("/chapter");
+  const isCoursePage = pathname?.includes("/course");
+  const isSearchPage = pathname === "/search";
 
   return (
-    <div className="flex items-center gap-x-2 ml-auto">
-      {isTeacherPage || isPlayerPage ? (
-          <Link href="/dashboard">
-        <Button size="sm" variant="ghost">
-          <ExitIcon className="h-4 w-4 mr-2" />
-          Exit
-        </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button size="sm" variant="ghost">
-            <BackpackIcon className="h-4 w-4 mr-2"/>
-            Teacher mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
       )}
-      <UserButton />
-    </div>
+      <div className="flex items-center gap-x-2 ml-auto">
+        {isTeacherPage || isCoursePage ? (
+          <Link href="/dashboard">
+            <Button size="sm" variant="ghost">
+              <ExitIcon className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              <BackpackIcon className="h-4 w-4 mr-2" />
+              Teacher mode
+            </Button>
+          </Link>
+        )}
+        <UserButton />
+      </div>
+    </>
   );
 };
