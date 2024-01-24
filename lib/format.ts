3376037ad644c00c1022de94aa2962b401a0be1c
jawherbalti <jawher-balti@live.com>
2024-01-24 17:24:16 +0100

    format reviews

--- lib/format.ts
+++ lib/format.ts
@@ -4,3 +4,29 @@ export const formatPrice = (price: number) => {
         currency: "USD"
     }).format(price)
 }
+
+export const formatTotalReviews = (reviews: number) => {
+    if (reviews >= 1000 && reviews < 1000000) {
+        return (reviews / 1000).toFixed(1) + 'K';
+    } else if (reviews >= 1000000) {
+        return (reviews / 1000000).toFixed(1) + 'M';
+    } else {
+        return reviews;
+    }
+}
+
+export const formatCourseReview = (courseReview: number) => {
+    return courseReview%1 === 0 ? courseReview : courseReview.toFixed(1)
+}
+
+// export const formatDate = (date:Date) => {
+//     const pad = (num: number) => String(num).padStart(2, '0');
+
+//     const day = pad(date.getDate());
+//     const month = pad(date.getMonth() + 1); // Months are 0-indexed in JavaScript
+//     const year = date.getFullYear();
+//     const hours = pad(date.getHours());
+//     const minutes = pad(date.getMinutes());
+
+//     return `${day}/${month}/${year} ${hours}:${minutes}`;
+// }
