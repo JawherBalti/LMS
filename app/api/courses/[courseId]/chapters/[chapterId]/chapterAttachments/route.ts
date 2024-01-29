81b13c730d797a4807bb56851fc73ab8f22e53a6
jawherbalti <jawher-balti@live.com>
2024-01-29 10:30:43 +0100

    chapter attachments api

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/chapterAttachments/route.ts
@@ -0,0 +1,33 @@
+import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request, { params }: { params: { courseId: string, chapterId: string } }) {
+    try {
+        const user = await currentUser()
+        const { url } = await req.json()
+
+        if (!user?.id) return new NextResponse("Unauthorized", { status: 401 })
+        const courseOwner = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                userId: user.id
+            }
+        })
+
+        if (!courseOwner) return new NextResponse("Unauthorized", { status: 401 })
+
+        const attachment = await db.chapterAttachment.create({
+            data: {
+                url,
+                name: url.split("/").pop(),
+                chapterId: params.chapterId
+            }
+        })
+
+        return NextResponse.json(attachment)
+
+    } catch (error) {
+        return new NextResponse("Internal Error", { status: 500 })
+    }
+}
