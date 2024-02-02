d93aa72b110b9c676b24ad5e5c61b846bbfda636
jawherbalti <jawher-balti@live.com>
2024-02-02 10:12:55 +0100

    reorder subchapters api

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/subChapters/reorder/route.ts
@@ -0,0 +1,34 @@
+import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PUT (req: Request, {params}:{params:{courseId: string, chapterId: string}}) {
+    try {
+        const user = await currentUser()
+        if (!user?.id) return new NextResponse("Unauthorized", { status: 401 })
+
+        const {list} = await req.json()
+
+        const courseOwner = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                userId: user.id
+            }
+        })
+        if (!courseOwner) return new NextResponse("Unauthorized", { status: 401 })
+
+        for(let item of list) {
+            await db.subChapter.update({
+                where: {
+                    id: item.id
+                },
+                data: {
+                    position: item.position
+                }
+            })
+        }
+        return new NextResponse("Success", {status: 200})
+    } catch(error) {
+        return new NextResponse("Internal Error", {status: 500})
+    }
+}
