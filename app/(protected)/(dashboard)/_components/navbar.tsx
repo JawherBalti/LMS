9e3d47346d93ef8f042139a5f9eab869cc92ea24
jawherbalti <jawher-balti@live.com>
2024-02-05 13:04:15 +0100

    remove unused usePathname hook

--- app/(protected)/(dashboard)/_components/navbar.tsx
+++ app/(protected)/(dashboard)/_components/navbar.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import MobileSidebar from "./mobile-sidebar";
 import { NavbarRoutes } from "@/components/navbar-routes";
 
 export const Navbar = () => {
-  const pathname = usePathname();
-
   return (
     <nav className="border-b p-4 h-full flex items-center bg-white dark:bg-background shadow-sm">
       <MobileSidebar />
