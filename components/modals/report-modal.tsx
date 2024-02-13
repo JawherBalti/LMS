412f9050255b9be60c16245568518d869d68ab56
jawherbalti <jawher-balti@live.com>
2024-02-13 12:05:05 +0100

    report modal

---
+++ components/modals/report-modal.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+
+interface ConfirmModalProps {
+  children: React.ReactNode;
+  radioGroup: React.ReactNode
+  onConfirm: () => void;
+}
+
+export const ReportModal = ({ children, radioGroup, onConfirm }: ConfirmModalProps) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Report user?</AlertDialogTitle>
+          <AlertDialogDescription className="text-foreground">
+            Reports will be reviewed by an admin. <br />
+            Reported users may have their account suspended. <br />
+            Reporting a user for no reason may lead to account termination.<br/>
+          {radioGroup}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
