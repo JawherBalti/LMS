d57726a692df1f4f48ea35350f9c422f03e67e49
jawherbalti <jawher-balti@live.com>
2024-02-19 11:14:25 +0100

    change name

--- app/(protected)/(dashboard)/_components/sidebar.tsx
+++ app/(protected)/(dashboard)/_components/sidebar.tsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
       <div className="px-4 sm:px-6 lg:px-8 py-4 flex items-end">
         <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
           <p className="font-bold text-3xl capitalize text-emerald-950 dark:text-foreground">
-            Etudet
+            Courset
           </p>
         </Link>
       </div>
