4e7d3a30bbff4215f981b99210954697e7019eed
jawherbalti <jawher-balti@live.com>
2024-03-13 11:25:27 +0100

    remove unnecessary imports from login form page

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,24 +1,24 @@
 "use client";
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { LoginSchema } from "@/schemas";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "../ui/form";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
-import { FormError } from "../form-error";
-import { FormSuccess } from "../form-success";
-import { login } from "@/actions/login";
-import Link from "next/link";
-import { useState, useTransition } from "react";
-import { useSearchParams } from "next/navigation";
+// import * as z from "zod";
+// import { useForm } from "react-hook-form";
+// import { LoginSchema } from "@/schemas";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "../ui/form";
+// import { Input } from "../ui/input";
+// import { Button } from "../ui/button";
+// import { FormError } from "../form-error";
+// import { FormSuccess } from "../form-success";
+// import { login } from "@/actions/login";
+// import Link from "next/link";
+// import { useState, useTransition } from "react";
+// import { useSearchParams } from "next/navigation";
 import { CardWrapper } from "./card-wrapper";
 
 export const LoginForm = () => {
