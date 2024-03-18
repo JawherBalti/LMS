8b02032c80e87e55fe5f92d5c8c5c2050e339545
jawherbalti <jawher-balti@live.com>
2024-03-18 16:45:12 +0100

    fix dark theme bug

--- app/(protected)/(admin)/admin/_components/admin-sidebar.tsx
+++ app/(protected)/(admin)/admin/_components/admin-sidebar.tsx
@@ -8,7 +8,7 @@ const AdminSidebar = () => {
     <div className="h-full border-r flex flex-col overflow-y-auto bg-white dark:bg-background shadow-sm">
       <div className="px-4 sm:px-6 lg:px-8 py-4 flex items-end">
         <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-          <p className="font-bold text-3xl capitalize text-emerald-500 dark:text-background">
+          <p className="font-bold text-3xl capitalize text-emerald-950 dark:text-foreground">
             Courset
           </p>
         </Link>
