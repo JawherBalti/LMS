35587a7be5763d5044ea2dbf492d6e4805f91a58
jawherbalti <jawher-balti@live.com>
2024-03-22 17:09:20 +0100

    add carousel component

---
+++ components/carousel-data.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export function CarouselData({data}: {data: number[]}) {
+  return (
+    <Carousel
+      opts={{
+        align: "center",
+      }}
+      className="p-8"
+    >
+      <CarouselContent>
+        {data.map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card className="">
+                <CardContent className="flex items-center justify-center p-6">
+                  <span className="text-3xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
