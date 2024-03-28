05e2acde094105644e1310734ba703b2ecdee422
jawherbalti <jawher-balti@live.com>
2024-03-28 15:02:40 +0100

    add recently created courses to home page carousel

---
+++ actions/get-recent-courses.ts
@@ -0,0 +1,52 @@
+import { db } from "@/lib/db";
+import { Category, Course, Review } from "@prisma/client";
+import { getProgress } from "./get-progress";
+
+type CourseWithCategory = Course & {
+    category: Category | null
+    chapters: { id: string }[]
+    reviews: Review[] | null
+    totalReviews: number
+    courseReview: number
+}
+
+export const getRecentCourses = async (): Promise<CourseWithCategory[]> => {
+    try {
+        const courses = await db.course.findMany({
+            include: {
+                category: true,
+                reviews: true,
+                chapters: {
+                    where: {
+                        isPublished: true
+                    },
+                    select: {
+                        id: true
+                    }
+                },
+            },
+            orderBy: {
+                createdAt: "desc"
+            },
+            // take: 6
+        })
+
+        const coursesWithProgress: CourseWithCategory[] = await Promise.all(
+            courses.map(async course => {
+                const totalReviews = course.reviews.length
+                const courseReview = totalReviews === 0 ? 0 : course.reviews.reduce((acc, curr) => acc + curr.review, 0) / totalReviews
+
+                return {
+                    ...course,
+                    totalReviews,
+                    progress: null,
+                    courseReview,
+                }
+            })
+        )
+     
+        return coursesWithProgress
+    } catch (error) {
+        return []
+    }
+}
