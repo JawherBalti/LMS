212032aa2418af53d497e10bf35e1bf5ec063a05
jawherbalti <jawher-balti@live.com>
2024-03-28 14:58:00 +0100

    hide hamburger button from navbar in home page

--- app/(protected)/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(protected)/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,13 +1,19 @@
+"use client";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import Sidebar from "./sidebar";
+import { usePathname } from "next/navigation";
 
 const MobileSidebar = () => {
+  const pathname = usePathname();
+  const isHomePage = pathname === "/";
   return (
     <Sheet>
-      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
-        <HamburgerMenuIcon />
-      </SheetTrigger>
+      {!isHomePage ? (
+        <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+          <HamburgerMenuIcon />
+        </SheetTrigger>
+      ) : null}
       <SheetContent side="left" className="p-0 bg-white">
         <Sidebar />
       </SheetContent>
