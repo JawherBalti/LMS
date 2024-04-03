e33bd4bda2247aeac68e25f49905b334db164c12
jawherbalti <jawher-balti@live.com>
2024-04-03 17:04:02 +0100

    added github login button

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -17,7 +17,7 @@ export const Social = () => {
     });
   };
   return (
-    <div className="flex items-center w-full gap-x-2">
+    <div className="flex flex-col items-center w-full gap-2">
       <Button
         size="lg"
         className="w-full"
@@ -27,14 +27,14 @@ export const Social = () => {
         <FcGoogle className="h-5 w-5" />
       </Button>
 
-      {/* <Button
+      <Button
         size="lg"
         className="w-full"
         variant="outline"
         onClick={() => onClick("github")}
       >
         <FaGithub className="h-5 w-5" />
-      </Button> */}
+      </Button>
     </div>
   );
 };
