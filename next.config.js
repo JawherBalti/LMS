1873c8988254fc2ba24e16e1e0ef4d7ef5c44d88
jawherbalti <jawher-balti@live.com>
2024-04-24 17:03:31 +0100

    add linkedin oauth provider

--- next.config.js
+++ next.config.js
@@ -1,13 +1,26 @@
+const { hostname } = require("os");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
         hostname: "utfs.io",
       },
       {
-        hostname: "lh3.googleusercontent.com"
-      }
+        hostname: "cdn.discordapp.com",
+      },
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        hostname: "avatars.githubusercontent.com",
+      },
     ],
   },
 };
