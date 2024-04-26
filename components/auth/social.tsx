b6883671198ff4ad46ed97eaa540f80948cf3b9a
jawherbalti <jawher-balti@live.com>
2024-04-26 15:32:32 +0100

    remove linkedin login button

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FaGithub, FaDiscord, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaDiscord } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "../ui/button";
 import { signIn } from "next-auth/react";
@@ -11,7 +11,9 @@ export const Social = () => {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl");
 
-  const onClick = (provider: "google" | "github" | "discord" | "wordpress" | "linkedin") => {
+  const onClick = (
+    provider: "google" | "github" | "discord" | "wordpress" | "linkedin"
+  ) => {
     signIn(provider, {
       callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
     });
@@ -42,14 +44,6 @@ export const Social = () => {
         onClick={() => onClick("discord")}
       >
         <FaDiscord className="h-5 w-5 text-[#7289da]" />
-      </Button>
-      <Button
-        size="lg"
-        className="w-full"
-        variant="outline"
-        onClick={() => onClick("linkedin")}
-      >
-        <FaLinkedin className="h-5 w-5" />
       </Button>
     </div>
   );
