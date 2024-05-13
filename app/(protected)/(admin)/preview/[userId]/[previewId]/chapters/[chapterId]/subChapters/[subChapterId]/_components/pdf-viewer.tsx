d430e5a6bfe1e1025f1c242e4608cf418b06b77d
jawherbalti <jawher-balti@live.com>
2024-05-13 11:39:56 +0100

    create pdf viewer component

---
+++ app/(protected)/(admin)/preview/[userId]/[previewId]/chapters/[chapterId]/subChapters/[subChapterId]/_components/pdf-viewer.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { useResizeObserver } from "@wojtekmaj/react-hooks";
+import { pdfjs, Document, Page } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
+// import './Sample.css';
+
+import type { PDFDocumentProxy } from "pdfjs-dist";
+import { ChapterAttachment } from "@prisma/client";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
+
+const options = {
+  cMapUrl: "/cmaps/",
+  standardFontDataUrl: "/standard_fonts/",
+};
+
+const resizeObserverOptions = {};
+
+const maxWidth = 800;
+
+type PDFFile = string | File | null;
+
+interface sampleProps {
+  subChapterAttachments: ChapterAttachment[];
+}
+
+export default function PdfViewer({ subChapterAttachments }: sampleProps) {
+  const [file, setFile] = useState<PDFFile>(subChapterAttachments[0].url);
+  const [numPages, setNumPages] = useState<number>();
+  const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>();
+
+  const onResize = useCallback<ResizeObserverCallback>((entries) => {
+    const [entry] = entries;
+
+    if (entry) {
+      setContainerWidth(entry.contentRect.width);
+    }
+  }, []);
+
+  useResizeObserver(containerRef, resizeObserverOptions, onResize);
+
+  function onDocumentLoadSuccess({
+    numPages: nextNumPages,
+  }: PDFDocumentProxy): void {
+    setNumPages(nextNumPages);
+  }
+
+  return (
+
+      <Document
+        file={file}
+        onLoadSuccess={onDocumentLoadSuccess}
+        options={options}
+        loading={"loading file"}
+        className="sm:w-auto md:w-full lg:w-fit m-auto aspect-video overflow-scroll "
+      >
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+            width={
+              containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth
+            }
+          />
+        ))}
+      </Document>
+  );
+}
