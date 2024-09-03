0446c42dd4db9de2e1e7b104ec65f0574983d3d5
jawherbalti <jawher-balti@live.com>
2024-09-03 15:48:17 +0100

    add back button to login page

--- components/auth/back-button.tsx
+++ components/auth/back-button.tsx
@@ -16,7 +16,7 @@ export const BackButton = ({ href, label }: BackButtonProps) => {
         size="sm"
         asChild
     >
-      {/* <Link href={href}>{label}</Link> */}
+      <Link href={href}>{label}</Link>
     </Button>
   );
 };
